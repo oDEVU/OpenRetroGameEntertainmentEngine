b01c9b580d4b46de4087d0117339532dc10a974d
DEVU <64399110+oDEVU@users.noreply.github.com>
2022-07-25 14:07:02 +0200

    Separated engine and game code to multiple files.
    Textures still are not optimilized. Top and bottom textures still not work.

---
+++ game.cpp
@@ -0,0 +1,147 @@
+#include <raylib.h>
+#include "engine/functions.hpp"
+    
+    //Textures --------------------------------------------------
+
+    Image bricks    = LoadImage("game/textures/bricks.png"); 
+    Image grass     = LoadImage("game/textures/grass.png"); 
+    Image stone     = LoadImage("game/textures/stone.png"); 
+    Image temp      = LoadImage("game/textures/temp.png"); 
+
+    void Unload(){
+        UnloadImage(bricks);  
+        UnloadImage(grass); 
+        UnloadImage(stone);  
+        UnloadImage(temp); 
+    }
+
+    //Texture Map ----------------------------------------------
+
+
+    Image get_texture_from_texturemap(int txt) {
+
+        Image texture;
+
+            if(txt == 1){
+                texture = bricks;
+            }else if(txt == 2){
+                texture = grass;
+            }else if(txt == 3){
+                texture = stone;
+            }else{
+                texture = temp;
+            }
+
+        return texture;
+    }
+
+    // map loader -----------------------------------------------
+
+    int num_sec = 4;
+    int num_wall = 16; 
+
+
+    int load_sectors[] =
+    { //ws,we,z1,z2,c1,c2,t,txtt,txtb
+        0,4,0,40,0,0,0,0,0,       //sector 1
+        4,8,0,40,2,2,0,0,0,       //sector 2
+        8,12,0,40,99,99,0,0,0,    //sector 3
+        12,16,0,40,99,99,1,2,3,   //sector 4
+    };
+
+    int load_walls[] =
+    {  //x1,y1,x2,y2,color,is textured,texture,
+        0,0,32,0,0,0,0,     //sector 1 * 4 walls
+        32,0,32,32,0,0,0,
+        32,32,0,32,0,0,0,
+        0,32,0,0,0,0,0,
+
+        64,0,96,0,2,0,0,    //sector 2 * 4 walls
+        96,0,96,32,2,0,0,
+        96,32,64,32,2,0,0,
+        64,32,64,0,2,0,0,
+
+        64,64,96,64,99,0,0,  //sector 3 * 4 walls
+        96,64,96,96,99,0,0,
+        96,96,64,96,99,0,0,
+        64,96,64,64,99,0,0,
+
+        0,64,32,64,69,1,1,   //sector 4 * 4 walls
+        32,64,32,96,69,1,1,
+        32,96,0,96,69,1,1,
+        0,96,0,64,69,1,1,
+    }; 
+
+    //Color Map ----------------------------------------------
+
+
+    Color get_color_from_colormap(int c) {
+
+        Color top{ 255, 255, 255, 255 };
+        
+        if (c == 0){
+             top = Color{ 255, 0, 0, 255 };
+        }else if (c== 1){
+             top = Color{ 155, 0, 0, 255 };
+        }else if (c == 2){
+             top = Color{ 0, 255, 0, 255 };
+        }else if (c == 3){
+             top = Color{ 0, 155, 0, 255 };
+        }else if (c == 4){
+             top = Color{ 0, 0, 255, 255 };
+        }else if (c == 5){
+             top = Color{ 0, 0, 155, 255 };
+        }else if (c == 6){
+             top = Color{ 255, 255, 0, 255 };
+        }else if (c == 7){
+             top = Color{ 155, 155, 0, 255 };
+        }else{
+             top = Color{ 255, 255, 255, 100 };
+        }//You can add new colors here :)
+
+        return top;
+    }
+
+    void init() {           // this function is called at start of the program
+
+    }
+
+    //Init stuff
+
+    //Player spawn position
+
+    int player_x = 0;     //Statick info
+    int player_y = 0;
+    int player_z = 0;
+    int player_a = 0;
+    int player_l = 0;
+
+    void init_player() {   //Updates static info at init (for example load pos from file)
+        player_x = 0;
+        player_y = 0;
+        player_z = 10;
+        player_a = 0;
+        player_l = 0;
+    }
+
+    const int size_of_dirs = 3;      //This is that way because of const char* :()
+
+    const char* dirs[size_of_dirs] {
+        "/screenshots",
+        "/configs",
+        "/game/textures"            //here start game related stuff
+    };
+
+    //Every frame stuff
+
+    int scrn_num_testing = 0;   //You need to use local variables not this in main
+
+    void tick() {           // this function is called every frame (dont over use it)
+        if (IsKeyPressed(KEY_F3)){
+            printf("F3 pressed!");
+        }
+    }
+
+    void draw_game() {      // this function is called every draw call (dont over use it) (you can use all raylib.h draw functions here)
+        //DrawPixel(69,69,WHITE);
+    }
--- main.cpp
+++ main.cpp
@@ -1,402 +1,47 @@
-
-#include "raylib.h"
-#include "rlgl.h"
+#include <raylib.h>
+#include <rlgl.h>
 #include <GL/glew.h>
 #include <GL/glut.h>
 #include <cstdio>
-#include <stdio.h>
+#include <stdio.h>                      // BLOAT    (    but i dont want to clear it :)    )
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
+#include <fstream>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <filesystem>
+//#include "functions.hpp"
 //#include <iostream>
 
 
     //Game things
 
-    Image bricks    = LoadImage("game/textures/bricks.png"); 
-    Image grass     = LoadImage("game/textures/grass.png"); 
-    Image stone     = LoadImage("game/textures/stone.png"); 
-    Image temp     = LoadImage("game/textures/stone.png"); 
+    #include "game.cpp"
 
-    void Unload(){
-        UnloadImage(bricks);  
-        UnloadImage(grass); 
-        UnloadImage(stone);  
-        UnloadImage(temp); 
-    }
+    //Engine things
 
-    int num_sec = 4;
-    int num_wall = 16;  
+    #include "engine/render.hpp"
 
     //--------------
 
-
-char* unconstchar(const char* s) {
-    if(!s)
-      return NULL;
-    int i;
-    char* res = NULL;
-    res = (char*) malloc(strlen(s)+1);
-    if(!res){
-        fprintf(stderr, "Memory Allocation Failed! Exiting...\n");
-        exit(EXIT_FAILURE);
-    } else{
-        for (i = 0; s[i] != '\0'; i++) {
-            res[i] = s[i];
-        }
-        res[i] = '\0';
-        return res;
-    }
-}
-
-float torad(float x){
-    float rad = (x * M_PI) / 180;
-    return rad;
-}
-
-typedef struct{
-    float x,y,z;  //Position
-    float a;      // angle l-r
-    float l;      // angle u-d
-}player; player P;
-
-typedef struct{
-    float x1,y1;
-    float x2,y2;
-    int c;
-    bool t;
-    int txt;
-}walls; walls W[64];
-
-typedef struct{
-    float ws,we;
-    float z1,z2;
-    float x,y;
-    float d;
-    int c1,c2;
-    bool t;
-    int txtt;
-    int txtb;
-    int surf[32000];
-    int surface;
-}sectors; sectors S[64];
-
-void clip_behind_camera(float *x1,float *y1, float *z1, float x2,float y2,float z2){
-    float da=*y1;
-    float db=y2;
-    //float d=da-db;
-    //if (d==0){d=1;};
-    float s = da / (da-db);
-    *x1 = *x1 + s*(x2-(*x1));
-    *y1 = *y1 + s*(y2-(*y1)); if(*y1==0){*y1=1;};
-    *z1 = *z1 + s*(z2-(*z1)); //if(*z1<=0){*z1=1;};
-}
-
-void draw_wall(int x1, int x2, int b1, int b2, int t1, int t2, int c, int s, int t, int txt, int w){
-    int x,y;
-
-    int dyb = b2-b1;
-    int dyt = t2-t1;
-    int dx = x2-x1;
-    if(dx==0){
-        dx=1;
-    }
-    int xs=x1;
-
-    int bx1 = x1;
-    int bx2 = x2;
-
-    if(x1<1){x1=1;}
-    if(x2<1){x2=1;}
-    if(x1>(GetScreenWidth()-1)){x1=(GetScreenWidth()-1);}
-    if(x2>(GetScreenWidth()-1)){x2=(GetScreenWidth()-1);}
-
-    for(int i=x1;i<x2;i++){
-        int y1 = dyb*(i-xs+0.5)/dx+b1;
-        int y2 = dyt*(i-xs+0.5)/dx+t1;
-
-        int by1 = y1;
-        int by2 = y2;
-
-        if(y1<1){y1=1;}
-        if(y2<1){y2=1;}
-        if(y1>(GetScreenHeight()-1)){y1=(GetScreenHeight()-1);}
-        if(y2>(GetScreenHeight()-1)){y2=(GetScreenHeight()-1);}
-
-        //DrawPixel(i,y1,WHITE);
-        //DrawPixel(i,y2,WHITE);
-        //for(int y=y1;y<y2;y++){
-        //    DrawPixel(i,y,WHITE);
-        //}
-
-        Color top{ 255, 255, 255, 255 };
-        Color bottom{ 255, 255, 255, 255 };
-        
-        if (S[s].c1 == 0){
-             top = Color{ 255, 0, 0, 255 };
-        }else if (S[s].c1 == 1){
-             top = Color{ 155, 0, 0, 255 };
-        }else if (S[s].c1 == 2){
-             top = Color{ 0, 255, 0, 255 };
-        }else if (S[s].c1== 3){
-             top = Color{ 0, 155, 0, 255 };
-        }else if (S[s].c1 == 4){
-             top = Color{ 0, 0, 255, 255 };
-        }else if (S[s].c1 == 5){
-             top = Color{ 0, 0, 155, 255 };
-        }else if (S[s].c1 == 6){
-             top = Color{ 255, 255, 0, 255 };
-        }else if (S[s].c1 == 7){
-             top = Color{ 155, 155, 0, 255 };
-        }else{
-             top = Color{ 255, 255, 255, 100 };
-        }
-        
-        if (S[s].c2 == 0){
-             bottom = Color{ 255, 0, 0, 255 };
-        }else if (S[s].c2 == 1){
-             bottom = Color{ 155, 0, 0, 255 };
-        }else if (S[s].c2 == 2){
-             bottom = Color{ 0, 255, 0, 255 };
-        }else if (S[s].c2 == 3){
-             bottom = Color{ 0, 155, 0, 255 };
-        }else if (S[s].c2 == 4){
-             bottom = Color{ 0, 0, 255, 255 };
-        }else if (S[s].c2 == 5){
-             bottom = Color{ 0, 0, 155, 255 };
-        }else if (S[s].c2 == 6){
-             bottom = Color{ 255, 255, 0, 255 };
-        }else if (S[s].c2 == 7){
-             bottom = Color{ 155, 155, 0, 255 };
-        }else{
-             bottom = Color{ 255, 255, 255, 100 };
-        }
-
-        if(S[s].surface == 1){S[s].surf[i]=y1; continue;}
-        if(S[s].surface == 2){S[s].surf[i]=y2; continue;}
-
-
-
-        Image tb = bricks;
-
-        if(S[s].txtt == 1){
-            tb = bricks;
-        }else if(S[s].txtt == 2){
-            tb = grass;
-        }else if(S[s].txtt == 3){
-            tb = stone;
-        }
-
-        Image bb = bricks;
-
-        if(S[s].txtb == 1){
-            bb = bricks;
-        }else if(S[s].txtb == 2){
-            bb = grass;
-        }else if(S[s].txtb == 3){
-            bb = stone;
-        }
-
-        if(S[s].surface == -1){
-            if(S[s].t==0){
-                DrawLine(i, S[s].surf[i], i, y1, bottom);
-                DrawLine(i, y2, i, S[s].surf[i], Color{255,255,255,50});
-            }else{
-                for(int y=S[s].surf[i];y<y1;y++){
-
-                    float ffy = by1 - S[s].surf[i];
-                    float ffx = bx2 - bx1;
-                    float ppy = (y-S[s].surf[i]) / ffy;
-                    float ppx = (i-bx1) / ffx;
-                    int iiy = floor(tb.height * ppy);
-                    int iix = floor(tb.width * ppx);
-                    Color bot = GetImageColor(tb, iix, iiy);
-
-                    DrawPixel(i,y,bot);
-                }
-            }
-        };
-
-        if(S[s].surface == -2){
-            if(S[s].t==0){
-                DrawLine(i, y2, i, S[s].surf[i], top);
-                DrawLine(i, S[s].surf[i], i, y1, Color{0,0,0,100});
-            }else{
-                for(int y=y2;y<S[s].surf[i];y++){
-
-                    float ffy = S[s].surf[i] - by2;
-                    float ffx = bx2 - bx1;
-                    float ppy = (y-by2) / ffy;
-                    float ppx = (i-bx1) / ffx;
-                    int iiy = floor(bb.height * ppy);
-                    int iix = floor(bb.width * ppx);
-                    Color tob = GetImageColor(bb, iix, iiy);
-
-                    DrawPixel(i,y,tob);
-                }
-            }
-        };
-
-        if(t==0){
-
-        Color wall_color{ 255, 255, 255, 255 };
-        
-        if (c == 0){
-             wall_color = Color{ 255, 0, 0, 255 };
-        }else if (c == 1){
-             wall_color = Color{ 255, 0, 255, 255 };
-        }else if (c == 2){
-             wall_color = Color{ 0, 255, 0, 255 };
-        }else if (c == 3){
-             wall_color = Color{ 0, 255, 255, 255 };
-        }else if (c == 4){
-             wall_color = Color{ 0, 0, 255, 255 };
-        }else if (c == 5){
-             wall_color = Color{ 0, 37, 155, 255 };
-        }else if (c == 6){
-             wall_color = Color{ 255, 255, 0, 255 };
-        }else if (c == 7){
-             wall_color = Color{ 155, 255, 0, 255 };
-        }else{
-             wall_color = Color{ 255, 255, 255, 100 };
-        }
-
-        DrawLine(i, y1, i, y2, wall_color);
-
-        }else if(t==1){
-
-            Image working = bricks;
-
-            if(txt == 1){
-                working = bricks;
-            }else if(txt == 2){
-                working = grass;
-            }else if(txt == 3){
-                working = stone;
-            }
-
-
-            for(int y=y1;y<y2;y++){
-
-                float fy = by2 - by1;
-                float fx = bx2 - bx1;
-                float py = (y-by1) / fy;
-                float px = (i-bx1) / fx;
-                int iy = floor(working.height * py);
-                int ix = floor(working.width * px);
-                if(iy<working.height && ix <working.width){
-                    Color col = GetImageColor(working, ix, iy);
-                    DrawPixel(i,y,col);
-                }else{
-                    DrawPixel(i,y,RED);
-                    DrawText(TextFormat("! ERROR: Unable to load color from coordinates"), 5, 240, 10, RED);
-                }
-            }
-        }
-
-        if((w%2)==0){
-
-        DrawLine(i, y1, i, y2, Color{0,0,0,50});
-        }
-
-    }
-}
-
-float dist(float x1,float y1, float x2, float y2){
-    float distance = sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-    return distance;
-}
-
-void Draw3D(){
-     float wx[4],wy[4],wz[4];
-     int s,w;
-     float CS=cos(torad(P.a)), SN=sin(torad(P.a));
-
-    for(s=0;s<num_sec-1;s++){
-        for(w=0;w<num_sec-s-1;w++){
-            if(S[w].d<S[w+1].d){
-                sectors st=S[w];
-                S[w]=S[w+1];
-                S[w+1]=st;
-            }
-        }
-    }
-
-     //sectors
-     
-     for(s=0;s<num_sec;s++){
-        S[s].d=0;
-
-        if(P.z<S[s].z1){S[s].surface=1;}
-        else if (P.z>S[s].z2){S[s].surface=2;}
-        else {S[s].surface=0;}
-
-        for(int loop=0;loop<2;loop++){
-            for(w=S[s].ws;w<S[s].we;w++){
-
-                 int x1=W[w].x1-P.x, y1=W[w].y1-P.y;
-                 int x2=W[w].x2-P.x, y2=W[w].y2-P.y;
-
-                 if(loop==0){int swp=x1; x1=x2;x2=swp;swp=y1;y1=y2;y2=swp;}
-
-    // if(x1 == 0){x1=1;};
-    // if(x2 == 0){x2=1;};
-    // if(y1 == 0){y1=1;};
-    // if(y2 == 0){y2=1;};
-
-                wx[0]=x1*CS-y1*SN;
-                wx[1]=x2*CS-y2*SN;
-                wx[2]=wx[0];
-                wx[3]=wx[1];
-
-                wy[0]=y1*CS+x1*SN;
-                wy[1]=y2*CS+x2*SN;
-                wy[2]=wy[0];
-                wy[3]=wy[1];
-
-                wz[0]=S[s].z1-P.z+((P.l*wy[0])/32.0);
-                wz[1]=S[s].z1-P.z+((P.l*wy[1])/32.0);
-                wz[2]=wz[0]+S[s].z2;
-                wz[3]=wz[1]+S[s].z2;
-                S[s].d+=dist(0,0,(wx[0]+wx[1])/2,(wy[0]+wy[1])/2);
-
-                if(wy[0]<=0 && wy[1]<=0){continue;};
-
-                if(wy[0]<=0){
-                    clip_behind_camera(&wx[0],&wy[0],&wz[0],wx[1],wy[1],wz[1]);
-                    clip_behind_camera(&wx[2],&wy[2],&wz[2],wx[3],wy[3],wz[3]);
-                }
-
-                if(wy[1]<=0){
-                    clip_behind_camera(&wx[1],&wy[1],&wz[1],wx[0],wy[0],wz[0]);
-                    clip_behind_camera(&wx[3],&wy[3],&wz[3],wx[2],wy[2],wz[2]);
-                }
-
-    //screen perspective stuff
-                wx[0]=wx[0]*200/wy[0]+(GetScreenWidth()/2); wy[0]=wz[0]*200/wy[0]+(GetScreenHeight()/2); 
-                wx[1]=wx[1]*200/wy[1]+(GetScreenWidth()/2); wy[1]=wz[1]*200/wy[1]+(GetScreenHeight()/2);  
-                wx[2]=wx[2]*200/wy[2]+(GetScreenWidth()/2); wy[2]=wz[2]*200/wy[2]+(GetScreenHeight()/2); 
-                wx[1]=wx[3]*200/wy[3]+(GetScreenWidth()/2); wy[3]=wz[3]*200/wy[3]+(GetScreenHeight()/2); 
-
-    //draw to screen
-    //if(wx[0]>0 && wx[0]<GetScreenWidth() && wy[0]>0 && wy[0]<GetScreenHeight()){
-    //DrawPixel(wx[0],wy[0],WHITE);
-    //}
-    //if(wx[1]>0 && wx[1]<GetScreenWidth() && wy[1]>0 && wy[1]<GetScreenHeight()){
-    //DrawPixel(wx[1],wy[1],WHITE);
-    //}
-
-                draw_wall(wx[0],wx[1],wy[0],wy[1],wy[2],wy[3],W[w].c, s , W[w].t, W[w].txt, w);
-            
-            }
-            S[s].d/=(S[s].we-S[s].ws);
-            S[s].surface*=-1;
-        }
-    }
-}
-
 int main()
 {
+    //int result = mkdir("/home/test.txt", 0777);
+
+    //SaveFileText("result.txt", unconstchar(TextFormat("%i",result)));  
+
+    for(int i=0;i<size_of_dirs;i++){
+    const char* engine_path = GetWorkingDirectory();
+    const char* dir = dirs[i];
+
+    std::string buf(engine_path);
+    buf.append(dir);
+
+    std::filesystem::create_directories(buf);
+    }  
+
+
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 
     int screenWidth = 800;
@@ -431,47 +76,18 @@ int main()
         vsync = false;
     }
 
-    // map loader
-
-    int load_sectors[] =
-    { //ws,we,z1,z2,c1,c2,t,txtt,txtb
-        0,4,0,40,0,0,0,0,0,       //sector 1
-        4,8,0,40,2,2,0,0,0,       //sector 2
-        8,12,0,40,99,99,0,0,0,    //sector 3
-        12,16,0,40,99,99,1,2,3,   //sector 4
-    };
-
-    int load_walls[] =
-    {  //x1,y1,x2,y2,color,is textured,texture,
-        0,0,32,0,0,0,0,     //sector 1 * 4 walls
-        32,0,32,32,0,0,0,
-        32,32,0,32,0,0,0,
-        0,32,0,0,0,0,0,
-
-        64,0,96,0,2,0,0,    //sector 2 * 4 walls
-        96,0,96,32,2,0,0,
-        96,32,64,32,2,0,0,
-        64,32,64,0,2,0,0,
-
-        64,64,96,64,99,0,0,  //sector 3 * 4 walls
-        96,64,96,96,99,0,0,
-        96,96,64,96,99,0,0,
-        64,96,64,64,99,0,0,
-
-        0,64,32,64,69,1,1,   //sector 4 * 4 walls
-        32,64,32,96,69,1,1,
-        32,96,0,96,69,1,1,
-        0,96,0,64,69,1,1,
-    };
-
 
     // INIT -----------------------------------------------------------------------
 
     InitWindow(screenWidth, screenHeight, "O.R.G.Y");
 
+    init();
+
             //create player
-        
-    P.x = 0; P.y = 0; P.z = 10; P.a = 0; P.l = 0;
+
+    init_player();
+
+    P.x = player_x; P.y = player_y; P.z = player_y; P.a = player_a; P.l = player_l;
 
     int s,w,v1=0,v2=0;
 
@@ -505,6 +121,8 @@ int main()
         // Update
         //----------------------------------------------------------------------------------
 
+        tick();
+
             //KeyInputs
         if (IsKeyPressed(KEY_TAB)){
             TakeScreenshot(unconstchar(TextFormat("screenshots/screen%i.png", screens_num)));
@@ -525,6 +143,12 @@ int main()
 
         if (IsKeyPressed(KEY_F2)){
             vsync = !vsync;
+
+            if (vsync == true){
+                SaveFileText("configs/vsync.cfg", unconstchar(TextFormat("on")));
+            }else{
+                SaveFileText("configs/vsync.cfg", unconstchar(TextFormat("")));
+            }
         }
 
             //player moving
@@ -600,10 +224,8 @@ int main()
 
         if (vsync == true){
             SetTargetFPS(GetMonitorRefreshRate(GetCurrentMonitor()));
-            SaveFileText("configs/vsync.cfg", unconstchar(TextFormat("on")));
         }else{
             SetTargetFPS(0);
-            SaveFileText("configs/vsync.cfg", unconstchar(TextFormat("")));
         }
 
         //----------------------------------------------------------------------------------
@@ -652,6 +274,8 @@ int main()
 
             // void DrawPoly(Vector2 center, int sides, float radius, float rotation, Color color);  
             // void DrawPixel(x,y,color); 
+
+            draw_game();
 
         EndDrawing();
         //----------------------------------------------------------------------------------
